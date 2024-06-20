dad5ad3f2c039234fb73da934c4da88978660e19
manana <manana@host.docker.internal>
2024-06-20 13:59:41 +0200

    Clase 20 /06

--- 15_gestor_productos/src/main/java/servlet/Buscar.java
+++ 15_gestor_productos/src/main/java/servlet/Buscar.java
@@ -1,23 +1,32 @@
 package servlet;
 
+import java.io.IOException;
+import java.util.List;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import model.Producto;
+import service.ProductosService;
 
-/**
- * Servlet implementation class Buscar
- */
+@WebServlet("/Buscar")
 public class Buscar extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		//Variable guarda input formulario
+		String categoria = request.getParameter("categoria");
+		
+		//Objeto q instancia el service
+		var service = new ProductosService();
+		//Lista en la que se guardan los resultados de la busqueda
+		List<Producto> productos = service.buscarPorCategoria(categoria);
+		//Guarda objeto en un atributo para el JSP lo pueda encontrar
+		request.setAttribute("productos", productos);
+		//transferir control a JSP
+		request.getRequestDispatcher("productosPorCategoria.jsp").forward(request, response);
 	}
 
 }
